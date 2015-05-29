eec02689757cf6dfdc4ed9ac129c71e882827734
Ahmad Nassri <ahmad@codeinchaos.com>
2015-05-29 19:25:54 -0400

    adding ability to use as a module + advanced configuration options

---
+++ test/index.js
@@ -0,0 +1,138 @@
+/* global describe, it */
+
+'use strict'
+
+var echint = require('..')
+var should = require('should')
+
+describe('echint', function () {
+  it('should use default values', function (done) {
+    echint().should.be.true
+
+    done()
+  })
+
+  it('should use list of files', function (done) {
+    var result = echint([
+      'test/fixtures/invalid',
+      'test/fixtures/invalid.js',
+      'test/fixtures/valid.js'
+    ], {
+      readPackage: false
+    })
+
+    result.should.be.false
+
+    done()
+  })
+
+  it('should match file pattern', function (done) {
+    var result = echint({
+      pattern: 'test/fixtures/*',
+      readPackage: false
+    })
+
+    result.should.be.false
+
+    done()
+  })
+
+  it('should read custom config file', function (done) {
+    var result = echint({
+      config: 'test/fixtures/.config',
+      pattern: 'test/fixtures/*',
+      readPackage: false
+    })
+
+    result.should.be.false
+
+    done()
+  })
+
+  it('should ignore files when using a pattern', function (done) {
+    var result = echint({
+      config: 'test/fixtures/.config',
+      ignore: ['test/fixtures/invalid*'],
+      pattern: 'test/fixtures/*',
+      readPackage: false
+    })
+
+    result.should.be.true
+
+    done()
+  })
+
+  it('should ignore none existant files', function (done) {
+    var result = echint([
+      'test/fixtures/fakefile'
+    ], {
+      config: 'test/fixtures/.config',
+      pattern: 'test/fixtures/*',
+      readPackage: false
+    })
+
+    result.should.be.true
+
+    done()
+  })
+
+  it('should ignore files when sending a list', function (done) {
+    var result = echint([
+      'test/fixtures/invalid'
+    ], {
+      config: 'test/fixtures/.config',
+      ignore: ['test/fixtures/invalid'],
+      pattern: 'test/fixtures/*',
+      readPackage: false
+    })
+
+    result.should.be.true
+
+    done()
+  })
+
+  it('should ignore none files', function (done) {
+    var result = echint([
+      'test/fixtures'
+    ], {
+      config: 'test/fixtures/.config',
+      pattern: 'test/fixtures/*',
+      readPackage: false
+    })
+
+    result.should.be.true
+
+    done()
+  })
+
+  it('should use callbacks', function (done) {
+    echint({
+      config: 'test/fixtures/.config',
+      ignore: ['test/fixtures/invalid*'],
+      pattern: 'test/fixtures/*',
+      readPackage: false
+    }, function (errors, valid) {
+      should.not.exist(errors)
+      valid.should.be.true
+
+      done()
+    })
+  })
+
+  it('should pass invalid files', function (done) {
+    echint({
+      config: 'test/fixtures/.config',
+      pattern: 'test/fixtures/*',
+      readPackage: false
+    }, function (errors, valid) {
+
+      valid.should.be.false
+      errors.should.be.an.Object
+
+      errors['test/fixtures/invalid'].should.be.an.Object
+      errors['test/fixtures/invalid.js'].should.be.an.Object
+
+      done()
+    })
+  })
+})
